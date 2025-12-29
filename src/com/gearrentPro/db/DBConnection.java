436ad37d784aeab5e357044560ff798bbb3e1b7e
savanisgithub <nilothma2000@gmail.com>
2025-12-29 21:29:21 +0530

    folder setup and connect db

---
+++ src/com/gearrentPro/db/DBConnection.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gearrentPro.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author User
+ */
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+    
+    private DBConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "Sava21#$");
+
+    }
+    
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+        if(dbConnection == null){
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+}
